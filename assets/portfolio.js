34a7d1318743f71ba4b073c23aa4587344103808
fish6206 <fish.greg4@gmail.com>
2018-12-19 21:18:44 -0800

    about me and concact form update

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -8,30 +8,36 @@ $('#sidebarBtn')
 
 //show portfolio button
 $(".show-port").on("click", function () {
-$("#title-button").empty().html("<h1>Portfolio</h1>").addClass("ui huge centered header");
-$("#div1").fadeIn(1000);
-$("#div2").fadeIn(2000);
-$("#hide-port").fadeIn(3000);
-$("#about-me").slideUp(800);
-$("#contact-form").fadeOut(2500);
+    $("#title-button").empty().html("<h1>Portfolio</h1>").addClass("ui huge centered header");
+    $("#div1").fadeIn(1000);
+    $("#div2").fadeIn(2000);
+    $("#hide-port").fadeIn(3000);
+    $("#about-me").slideUp(800);
+    $("#comp-img").fadeOut(2000);
+    $("#help-div").fadeOut(2000);
+    $("#contact-form").fadeOut(2000);
 });
 //hide portfolio button
 $("#hide-port").on("click", function () {
-$("#div1").slideUp(2000);
-$("#div2").slideUp(1000);
-$("#hide-port").fadeOut();
-$("#title-button").empty().html("<button>View Portfolio</button>");
-$("#about-me").slideDown(3500);
-$("#contact-form").fadeIn(4000);
+    $("#div1").slideUp(2000);
+    $("#div2").slideUp(1000);
+    $("#hide-port").fadeOut();
+    $("#title-button").empty().html("<button>View Portfolio</button>");
+    $("#about-me").slideDown(3500);
+    $("#comp-img").fadeIn(4000);
+    $("#help-div").fadeIn(4000);
+    $("#contact-form").fadeIn(4000);
 });
 //about me sidebar
 $("#hide-port-bar").on("click", function () {
-$("#div1").slideUp(2000);
-$("#div2").slideUp(1000);
-$("#hide-port").fadeOut();
-$("#title-button").empty().html("<button>View Portfolio</button>");
-$("#about-me").slideDown(3500);
-$("#contact-form").fadeIn(4000);
+    $("#div1").slideUp(2000);
+    $("#div2").slideUp(1000);
+    $("#hide-port").fadeOut();
+    $("#title-button").empty().html("<button>View Portfolio</button>");
+    $("#comp-img").fadeIn(4000);
+    $("#help-div").fadeIn(4000);
+    $("#about-me").slideDown(3500);
+    $("#contact-form").fadeIn(4000);
 });
 
 //semantic ui card
